7792cce658371cd34a82860a78bb6ba217620bf0
khoa0905 <nbdk090505@gmail.com>
2026-05-07 19:52:35 +0700

    fix: sw

--- src/frontend/dev-dist/sw.js
+++ src/frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.knogd7fgd9c"
+    "revision": "0.5e8qm0l9qvs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
